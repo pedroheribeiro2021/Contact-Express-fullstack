e72d47b6234be31d183bebd8c0c2f1b2a1d3856e
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-03-23 19:43:06 -0300

    fix: get user id by params

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createUserController, deleteUserController, listUsersController, updateUserController } from "../controllers/user.controller";
+import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
 
 
 export const userRoutes = Router()
 
-userRoutes.get('', listUsersController)
+// userRoutes.get('', listUsersController)
+userRoutes.get('/:id', ensureAuthMiddleware, listUsersController)
 userRoutes.post('', createUserController)
 userRoutes.patch('/:id', updateUserController)
 userRoutes.delete('/:id', deleteUserController)
--- backend/src/services/user/listUser.service.ts
+++ backend/src/services/user/listUser.service.ts
@@ -3,11 +3,14 @@ import { User } from "../../entities/client.user.entity"
 
 
 
-export const listUsersServices = async (): Promise<any> => {
+export const listUsersServices = async (userId: string): Promise<any> => {
 
     const userRepository = AppDataSource.getRepository(User)
 
     const user = await userRepository.find({
+        where: {
+            id: userId
+        },
         relations: {
             contacts: true
         }
