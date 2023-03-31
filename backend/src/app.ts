c81957feb5df57b0fc70b2e9671795f5f46173f9
Pedro Ribeiro <86488494+pedroheribeiro2021@users.noreply.github.com>
2023-03-31 14:56:28 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,11 @@ import { sessionRoutes } from './routes/session.routes'
 import { userRoutes } from './routes/user.routes'
 import { handleError } from "./errors/handleError"
 import { contactsRoutes } from "./routes/contacts.routes";
+import cors from "cors";
 
 export const app: Application = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/user', userRoutes)
 app.use('/login', sessionRoutes)
